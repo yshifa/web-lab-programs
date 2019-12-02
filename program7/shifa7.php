53dd01ee12fada7f5b92d1b682b6533cee38db41
yshifa <shifayaara050@gmail.com>
2019-12-03 03:30:12 +0530

    commited

---
+++ program7/shifa7.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+    <head>
+        <meta http-equiv="refresh" content="1"/>
+        <style>
+            p{
+                color:white;
+                font-size:90px;
+                positon:absolute;
+                top:50%;
+                left:50%;
+                transform:translate(-50%,-50%);
+            }
+            body{background-color:black;}
+            </style>
+            <p><?php echo date("h:i:s A")?></p>
+        </head>
+        <html>
